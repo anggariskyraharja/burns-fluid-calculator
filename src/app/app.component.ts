17dc1fc17043abc5dc630c3b777e17ea68dd4545
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2020-12-09 18:57:51 +0700

    iOS final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
   ) {
     this.initializeApp();
     // Initialize AdMob for your Application
-    AdMob.initialize("ca-app-pub-9580735978410872~4601898608");
+    AdMob.initialize("ca-app-pub-9580735978410872~7558780496");
   }
 
   initializeApp() {
